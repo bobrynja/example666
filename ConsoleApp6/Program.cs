3b7200f2d752b4ae7e5840715dc6420fcd6ca8a9
nastya <na@i2g.ru>
2024-04-19 07:27:35 +0300

    gfdcsx

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+
+
+
+
+    // определение класса Person
+    class Person
+    {
+        public string name = "Undefined";
+        public int age;
+
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {name}  Возраст: {age}");
+        }
+    }
+}
+    }
+}
