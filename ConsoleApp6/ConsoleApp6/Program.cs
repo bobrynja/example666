ac30604d309396155e0bc55414ffe172afce63e1
bobrynja <119498240+bobrynja@users.noreply.github.com>
2024-04-19 07:08:58 +0300

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+
+
+    class UserInfo
+    {
+        // Поля класса
+        public string Name, Family, Adress;
+        public byte Age;
+
+        // Метод, выводящий в консоль контактную информацию
+        public void writeInConsoleInfo(string name, string family, string adress, byte age)
+        {
+            Console.WriteLine("Имя: {0}\nФамилия: {1}\nМестонахождение: {2}\nВозраст: {3}\n", name, family, adress, age);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInfo myInfo = new UserInfo();
+
+            myInfo.Name = "Alexandr";
+            myInfo.Family = "Erohin";
+            myInfo.Adress = "ViceCity";
+            myInfo.Age = 26;
+
+            // Создадим новый экземпляр класса UserInfo
+            UserInfo myGirlFriendInfo = new UserInfo();
+
+            myGirlFriendInfo.Name = "Elena";
+            myGirlFriendInfo.Family = "Korneeva";
+            myGirlFriendInfo.Adress = "ViceCity";
+            myGirlFriendInfo.Age = 22;
+
+            // Выведем информацию в консоль
+            myInfo.writeInConsoleInfo(myInfo.Name, myInfo.Family, myInfo.Adress, myInfo.Age);
+            myGirlFriendInfo.writeInConsoleInfo(myGirlFriendInfo.Name, myGirlFriendInfo.Family, myGirlFriendInfo.Adress, myGirlFriendInfo.Age);
+
+            Console.ReadLine();
+        }
+    }
+}
